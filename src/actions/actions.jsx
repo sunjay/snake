bd5baa003b2579c5796a7cfe8d962100d8e71e58
Sunjay Varma <varma.sunjay@gmail.com>
2016-04-19 23:46:17 -0400

    Removing the key action since directions will now be used instead

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,14 +1,9 @@
 export const ACTION_RESET = 'reset';
-export const ACTION_KEY = 'key';
 export const ACTION_DIRECTION = 'direction';
 export const ACTION_ENABLE_AI = 'ai-enabled';
 
 export function resetGame() {
   return createAction(ACTION_RESET);
-}
-
-export function pressKey(key) {
-  return createAction(ACTION_KEY, {key});
 }
 
 export function snakeDirection(directionName) {
