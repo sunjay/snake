307d656c0492c961cae82a0c6ac9a20ab1cc47aa
Sunjay Varma <varma.sunjay@gmail.com>
2016-04-19 22:34:00 -0400

    Reducer for reset action

--- src/reducers/game.jsx
+++ src/reducers/game.jsx
@@ -2,8 +2,13 @@ const {createReducer} = require('./reducer');
 
 const SnakeGame = require('../models/snakeGame');
 
+const {ACTION_RESET} = require('../actions/actions');
+
 const initialState = SnakeGame.fromDimensions({rows: 30, cols: 30});
 const game = createReducer(initialState, {
+  [ACTION_RESET]() {
+    return initialState.placeRandomGoal();
+  },
 });
 
 module.exports = game;
