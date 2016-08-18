cce12b8b9738399368c03a551b732dfde51333bd
Sunjay Varma <varma.sunjay@gmail.com>
2016-08-18 02:07:30 -0400

    Implementing more of traversable space + pseudo code

--- src/models/traversableSpace.jsx
+++ src/models/traversableSpace.jsx
@@ -23,6 +23,23 @@ class Space extends SpaceRecord {
   get area() {
     return this.width * this.height;
   }
+
+  contains({x, y}) {
+    return (
+      x >= this.topLeft.x && x <= this.bottomRight.x &&
+      y >= this.topLeft.y && y <= this.bottomRight.y
+    );
+  }
+
+  /**
+   * Return a resized space that avoids the given point
+   * The returned space is guarenteed to not contain the given point
+   * 
+   * Assumes the given point is within this space
+   */
+  avoid({x, y}) {
+    return null;
+  }
 }
 
 const TraversableSpaceRecord = Record({
@@ -51,11 +68,44 @@ class TraversableSpace extends TraversableSpaceRecord {
    * Fill in the given space as not traversable
    */
   fill({x, y}) {
-    // find the space that contains this point
-    // resize that space to no longer contain this point by doing as small of a readjustment as possible
-    // resize the adjacents to fill in the extra traversable space
-    // add new adjacents as necessary to fill in any leftovers
-    return this;
+    // We only want to update the space that contains this point
+    // as well as the spaces around it
+    const spaceIndex = this.spaces.findIndex((sp) => sp.contains({x, y}));
+
+    // nothing to do since the point isn't in any of the traversable space
+    if (spaceIndex < 0) {
+      return this;
+    }
+
+    const space = this.spaces.get(spaceIndex);
+
+    // No matter where the given point is, we can resize
+    // the containing space to fit it
+    const resizedSpace = space.avoid({x, y});
+
+    // If the space only had area 1 to begin with, resizing it will simply
+    // remove the space entirely
+    let spaces;
+    if (resizedSpace === null) {
+      spaces = this.spaces.remove(spaceIndex);
+    }
+    else {
+      const adjacents = [];
+
+      // TODO: resize the adjacents to fill in the extra traversable space
+      // Adjacents can only be resized if they can be contained in
+      // either the vertical or horizontal dimension of the *original*
+      // space. i.e. spaces with mismatched dimensions cannot be used
+      // here
+      // Need to use the *original* space since these adjacents may not be
+      // contained in the resized version
+
+      // TODO: add new adjacents as necessary to fill in any leftovers
+
+      spaces = this.spaces.set(spaceIndex,
+        resizedSpace.set('adjacents', List.of(...adjacents)));
+    }
+    return this.set('spaces', spaces);;
   }
 
   /**
